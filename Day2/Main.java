2469769dccf3ccbe2811d91d381a0aee53512327
Mickey <1306shivam@gmail.com>
2024-12-24 16:34:59 +0530

    datatypes

---
+++ Day2/Main.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+    
+    
+    
+        System.out.println("Shivam"); //for next line instead of \n we are using println
+        System.out.print("Shivam\n");
+
+        //variables and datatypes
+        int a=5;
+        int b=6;
+        System.out.println(a*b);
+        String temp = "hello world";
+        System.out.println(temp);
+        double price = 25.233;
+        System.out.println(price);
+
+        //taking input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        // nextInt();
+        // nextFloat();
+        System.out.println(name);
+
+
+        int aa = sc.nextInt();
+        int bb = sc.nextInt();
+        System.out.println(aa+bb);
+    
+
+
+
+
+    }
+}
